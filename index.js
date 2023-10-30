a58775fb539a5818c6a2dff06d3588e755308989
Aluno FTT 02 <alunoftt02@aeelabs.edu.br>
2023-10-30 17:00:55 -0300

    att

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 const handlebars = require('express-handlebars')
 
 //config
-    // template engine
+    // template engine para o handlebars
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//conexão com o bando de dados
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+// rotas
+app.get('/cad', function(req, res){
+    res.render('formulario')  //renderizar a pagina formulario
+})
+
+app.post('/add', function(req, res){ //não tem como acessar rotas com o post pela URL
+    res.send("Form recebido")
+})
+
+app.get("/", function(req,res){
+    res.send("Ola")
+})
+
+app.listen(8081, function(){
+    console.log("Servido etá rodando na porta: https://LocalHost:8081")
+})  
